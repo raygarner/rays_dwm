e0db90fac78a622cc85be70ad63b46644fca7a47
raygarner <ray@perfectcast.co.uk>
2020-08-23 17:31:27 +0100

    simplified colours

--- config.h
+++ config.h
@@ -12,13 +12,14 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_white[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
 static const char col_green[]       = "#264d00";
 static const char col_clear[]	    = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_clear },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_clear  },
+	[SchemeSel]  = { col_white, col_gray1,  col_clear  },
 };
 
 /* tagging */
